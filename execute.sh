021b8d51410936cd5e6446e0bd506f1bb31961ad
SasikaSankalana <79664299+SasikaSankalana@users.noreply.github.com>
2023-10-21 14:40:27 +0530

    check

--- execute.sh
+++ execute.sh
@@ -1,8 +1,36 @@
-# sudo apt update
-# sudo apt install postfix dovecot-imapd
+#!/bin/bash
+
+while getopts "c:b:" opt; do
+  case $opt in
+    c)
+      catapults="$OPTARG"
+      ;;
+    b)
+      battalions="$OPTARG"
+      ;;
+  esac
+done
+
+IFS=' ' read -r -a battalion_array <<< "$battalions"
+unset IFS
+IFS=$'\n' battalion_array_sort=($(sort -nr <<<"${battalion_array[*]}"))
+unset IFS
+
+destroyed=0
+
+while [ "$catapults" -gt 0 ] && [ ${#battalion_array_sort[@]} -gt 0 ]; do
+  battalion=${battalion_array_sort[0]}
+
+  if [ "$catapults" -ge "$battalion" ]; then
+    catapults=$((catapults - battalion))
+    destroyed=$((destroyed + 1))
+  elif [ ${#battalion_array_sort[@]} -eq 1 ]; then
+    break
+  fi
+
+  battalion_array_sort=("${battalion_array_sort[@]:1}") 
+done
+
+echo "$destroyed"
 
 
-python_script="mail.py"
-
-# Run the Python script with sudo privileges
-sudo python3 "$python_script"
--- test/index.test.js
+++ test/index.test.js
@@ -1,27 +1,49 @@
-const imaps = require('imap-simple');
+const fs = require('fs');
+const { faker } = require('@faker-js/faker');
+const exec = require('@sliit-foss/actions-exec-wrapper').default
+const { scan, shellFiles, dependencyCount, prohibitedCommands, restrictJavascript, restrictPython } = require('@sliit-foss/bashaway');
 
-test('should validate if a mail is present in the inbox', async () => {
-    let debugOutput = '';
-    await new Promise((resolve) => {
-        imaps.connect({
-            imap: {
-                user: "inventor@bashaway2k23.net",
-                password: "123456",
-                host: 'localhost',
-                port: 1636,
-                debug: (debug) => {
-                    debugOutput += debug;
-                }
+jest.setTimeout(180000);
+
+test('should validate if only bash files are present', () => {
+    const shellFileCount = shellFiles().length;
+    expect(shellFileCount).toBe(1);
+    expect(shellFileCount).toBe(scan('**', ['src/**']).length);
+});
+
+test('should check if the destroyed battalion count is obtained correctly', async () => {
+    for (let i = 0; i < 8; i++) {
+        let catapults = faker.number.int({ min: 1, max: 75 });
+        const battalions = Array.from({ length: faker.number.int({ min: 1, max: 25 }) }, () => faker.number.int({ min: 1, max: 10 }));
+        const destroyedBatallions = await exec(`bash execute.sh -c ${catapults} -b \'${battalions.join(' ')}\'`);
+        const sortedBattalions = battalions.sort((a, b) => b - a);
+        let destroyed = 0;
+        while (catapults > 0) {
+            const battalion = sortedBattalions.shift();
+            if (catapults >= battalion) {
+                catapults -= battalion;
+                destroyed += 1;
+            } else if (sortedBattalions.length === 0) {
+                break;
             }
-        }).then(function (connection) {
-            return connection.openBox('INBOX').then(function () {
-                return connection.search(['UNSEEN'], {}).then(()=>{
-                    connection.end();
-                    resolve(debugOutput);
-                });
-            });
-        });
-    })
-    expect(debugOutput).toContain("1 EXISTS");
-    expect(debugOutput).toContain("[UNSEEN 1] Message 1 is the first unseen");
+        }
+        expect(Number(destroyedBatallions?.trim())).toBe(destroyed);
+    }
+});
+
+describe('should check installed dependencies', () => {
+    let script
+    beforeAll(() => {
+        script = fs.readFileSync('./execute.sh', 'utf-8')
+    });
+    test("javacript should not be used", () => {
+        restrictJavascript(script)
+    });
+    test("python should not be used", () => {
+        restrictPython(script)
+    });
+    test("no additional npm dependencies should be installed", async () => {
+        await expect(dependencyCount()).resolves.toStrictEqual(4)
+        expect(script).not.toMatch(prohibitedCommands);
+    });
 });
