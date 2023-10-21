86298c3a3b78741c3233778216ac6490d408c903
SasikaSankalana <79664299+SasikaSankalana@users.noreply.github.com>
2023-10-21 13:32:33 +0530

    test

---
+++ test/index.test.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const exec = require("@sliit-foss/actions-exec-wrapper").default;
+const { scan, shellFiles, ghOrgRepos } = require("@sliit-foss/bashaway");
+
+jest.setTimeout(120000);
+
+let dir;
+
+beforeAll(() => {
+  dir = process.cwd();
+});
+
+test("should validate if only bash files are present", () => {
+  const shellFileCount = shellFiles().length;
+  expect(shellFileCount).toBe(1);
+  expect(shellFileCount).toBe(scan("**", ["src/**"]).length);
+});
+
+test("all organization repos must be cloned properly", async () => {
+  //   await exec("bash ./execute.sh sliit-foss ./out");
+  //   await exec('bash -c "./execute.sh sliit-foss ./out"');
+  //   await exec('bash -c "./execute.sh sliit-foss ./out"');
+  const repos = await ghOrgRepos("sliit-foss");
+  const clonedRepos = fs.readdirSync(`out`);
+  expect(repos.length).toBe(clonedRepos.length);
+  for (const repo of repos) {
+    process.chdir(`out/${repo.name}`);
+    const actual = await exec(
+      'bash -c "basename $(git remote get-url origin)"'
+    );
+    expect(actual.trim().replace(new RegExp(".git$"), "")).toStrictEqual(
+      repo.name
+    );
+    process.chdir(dir);
+  }
+});
